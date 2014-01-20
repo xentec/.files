0d76a2d940aac0e83306214665718f6721950901
Xentec <xentec@aix0.eu>
2014-01-20 18:50:35 +0100

    awesome: use check for errors before restarting

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -44,7 +44,7 @@ local globalkeys = awful.util.table.join(
 
 		-- Standard program
 		awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-		awful.key({ modkey, "Control" }, "r", awesome.restart),
+		awful.key({ modkey, "Control" }, "r", awful.util.restart),
 		awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 		awful.key({ modkey,	          }, "q", function() awful.util.spawn(browser) end),
 
