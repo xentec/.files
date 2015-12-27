1d2e526eb3ff872ff30b024d8a61c4414ace29b0
xentec <artificial.i7@gmail.com>
2015-12-27 13:39:02 +0100

    awesome: added sticky key

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -184,6 +184,8 @@ local clientkeys = awful.util.table.join(
 			  {description = "move to screen", group = "client"}),
 	awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
 			  {description = "toggle keep on top", group = "client"}),
+	awful.key({ modkey, "Shift"   }, "t",      function (c) c.sticky = not c.sticky          end,
+			  {description = "toggle keep on top", group = "client"}),
 	awful.key({ modkey,           }, "n",
 		function (c)
 			-- The client currently has the input focus, so it cannot be
